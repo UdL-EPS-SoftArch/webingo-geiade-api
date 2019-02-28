3a1fee8119cc64cf3bfaf43c61e40033d2ca8622
danic96 <dani@example.com>
2019-02-28 16:52:44 +0100

    crete game 2 commit

---
+++ src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/steps/CreateGameStepDef.java
@@ -0,0 +1,9 @@
+package cat.udl.eps.entsoftarch.webingogeiadeapi.steps;
+
+import cucumber.api.java.en.When;
+
+public class CreateGameStepDef {
+    @When("^I register a new game$")
+    public void iRegisterANewGame() {
+    }
+}
