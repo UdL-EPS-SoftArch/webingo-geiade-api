27a0be48f64e6ffe9827744d5ba829b416b2be97
Victor Ferrer <viicc6@MacBook-Pro-de-Victor.local>
2019-03-06 16:50:51 +0100

    nothing changed

--- src/main/java/cat/udl/eps/entsoftarch/webingogeiadeapi/domain/Game.java
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiadeapi/domain/Game.java
@@ -3,8 +3,8 @@ package cat.udl.eps.entsoftarch.webingogeiadeapi.domain;
 import java.util.Collection;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Transient;
+import javax.smartcardio.Card;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,15 +13,11 @@ import org.springframework.security.core.authority.AuthorityUtils;
 
 @Entity
 public class Game {
-    @Id
     private int id;
     private int [] nums;
     // private List<Card> cardsInGame, players;
     private double linePrice, bingoPrice;
     private Player lineWinner, bingoWinner;
 
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_PLAYER");
-    }
 
 }
