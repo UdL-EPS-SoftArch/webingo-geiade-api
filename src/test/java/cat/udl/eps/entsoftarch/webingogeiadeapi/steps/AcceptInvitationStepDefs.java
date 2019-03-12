ba184e50ef5c1780a9ddfab8267bde6448c78d74
davidcalavia <davidcalavia2@gmail.com>
2019-03-12 17:15:44 +0100

    'Scenarios'

--- src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/steps/AcceptInvitationStepDefs.java
+++ src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/steps/AcceptInvitationStepDefs.java
@@ -5,13 +5,13 @@ import cucumber.api.java.en.When;
 
 public class AcceptInvitationStepDefs {
     @When("^I accept a new invitation with username \"([^\"]*)\", email \"([^\"]*)\", password \"([^\"]*)\" and game id \"([^\"]*)\"$")
-    public void iAcceptANewInvitationWithUsernameEmailPasswordAndGameId(String arg0, String arg1, String arg2, String arg3) throws Throwable {
+    public void iAcceptANewInvitation(String arg0, String arg1, String arg2, String arg3) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @When("^I reject a new invitation with username \"([^\"]*)\", email \"([^\"]*)\", password \"([^\"]*)\" and game id \"([^\"]*)\"$")
-    public void iRejectANewInvitationWithUsernameEmailPasswordAndGameId(String arg0, String arg1, String arg2, String arg3) throws Throwable {
+    public void iRejectANewInvitation(String arg0, String arg1, String arg2, String arg3) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
--- src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/steps/RegisterPlayerStepDef.java
+++ src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/steps/RegisterPlayerStepDef.java
@@ -16,7 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
-public class RegisterPlayerStepDef {
+public class git RegisterPlayerStepDef {
 
   private static final Logger logger = LoggerFactory.getLogger(RegisterPlayerStepDef.class);
 
