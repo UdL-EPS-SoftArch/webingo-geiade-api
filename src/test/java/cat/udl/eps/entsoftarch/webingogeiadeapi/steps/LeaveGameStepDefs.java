575028963a79337663ca05133d76f74ce84eda83
mireiamoix <mma20@alumnes.udl.cat>
2019-03-16 18:42:24 +0100

    No acabat

---
+++ src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/steps/LeaveGameStepDefs.java
@@ -0,0 +1,9 @@
+package cat.udl.eps.entsoftarch.webingogeiadeapi.steps;
+
+import cucumber.api.java.en.When;
+
+public class LeaveGameStepDefs {
+    @When("^I leave a game when I'm playing$")
+    public void iLeaveAGameWhenIMPlaying() {
+    }
+}
