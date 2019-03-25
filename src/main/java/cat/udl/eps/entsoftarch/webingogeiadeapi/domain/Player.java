0bea146dd9a4587ee81ac3e753d4a6848ffa8c0d
danic96 <dani@example.com>
2019-03-25 19:03:11 +0100

    card and game added to player

--- src/main/java/cat/udl/eps/entsoftarch/webingogeiadeapi/domain/Player.java
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiadeapi/domain/Player.java
@@ -2,8 +2,7 @@ package cat.udl.eps.entsoftarch.webingogeiadeapi.domain;
 
 import java.util.Collection;
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.security.core.GrantedAuthority;
@@ -15,7 +14,12 @@ import org.springframework.security.core.authority.AuthorityUtils;
 public class Player extends User {
 	private int wallet;
 	private boolean isPlaying;
-	// private List<Game> hasPlayed;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(unique = true)
+	private Game game;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(unique = true)
+	private Card card;
 
 	@Override
 	@Transient
