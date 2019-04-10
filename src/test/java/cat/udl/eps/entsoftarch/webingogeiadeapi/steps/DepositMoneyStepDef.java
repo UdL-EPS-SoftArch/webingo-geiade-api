a7e1b841fe4b82f06494c2008d6c97d51f6be3f5
danic96 <dani@example.com>
2019-04-10 15:42:28 +0200

    solved error with get-patch-get

--- src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/steps/DepositMoneyStepDef.java
+++ src/test/java/cat/udl/eps/entsoftarch/webingogeiadeapi/steps/DepositMoneyStepDef.java
@@ -42,23 +42,18 @@ public class DepositMoneyStepDef {
     @And("^\"([^\"]*)\" wallet is (\\d+)$")
     public void walletIs(String username, int cash) throws Throwable {
         this.player = (Player) playerRepository.findByEmail(username);
-        // UNAUTHORIZED ERROR AFTER PATCH
-        /*stepDefs.result = stepDefs.mockMvc.perform(
+
+        stepDefs.result = stepDefs.mockMvc.perform(
                 get("/players/{username}", this.player.getUsername())
                         .accept(MediaType.APPLICATION_JSON)
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.wallet", is(cash)));*/
-        if (this.player.getWallet() != cash) {
-            throw new Throwable();
-        }
+                .andExpect(jsonPath("$.wallet", is(cash)));
     }
 
     @When("^I deposit (\\d+) euros in \"([^\"]*)\"$")
     public void asIDepositEurosIn(int cash, String username) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        // AQUI S'HAURIA D?AFEGIR UN PATCH/PUT O AMB EL REPOSITORY
 
         this.player = (Player) playerRepository.findByEmail(username);
 
